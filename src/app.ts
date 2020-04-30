5a30d5be2f8af181b729d7e93e14dbd43d20b783
SurienDG <surien.das-giwojno@uwaterloo.ca>
2020-04-30 01:04:44 -0400

    Add outline for get routes

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const port = 3000;
 
 import https from 'https';
 import bodyParser from 'body-parser';
-import {validate, validateAndMakeMove} from 'chess_functions';
+import { validate, validateAndMakeMove } from 'chess_functions';
 
 
 app.use(bodyParser.json());
@@ -16,8 +16,18 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
-    res.json('tests');
+app.get('/ExistingGames', (req, res) => {
+    const userid = req.body;
+    // do data base query to get gameids for user\
+
+    // res.json(// put array of game id objects here)
+});
+
+app.get('/ExistingGame', (req, res) => {
+    const { userid, gameid } = req.body;
+    // do data base query user using user id and game id
+
+    // res.json(// put game id & FEN object)
 });
 
 
