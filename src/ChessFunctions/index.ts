89344bd797b38b2f77345c052c55e9016525310f
SurienDG <surien.das-giwojno@uwaterloo.ca>
2020-05-05 23:34:37 -0400

    Simplify index.ts file code for chess functions

--- src/ChessFunctions/index.ts
+++ src/ChessFunctions/index.ts
@@ -2,23 +2,14 @@ import bindings from 'bindings';
 
 
 const chessFunctions: {
-    validate_cPlus: (FEN: string, resolve: any, reject: any) => void,
-    validateAndMakeMove_cPlus: (FEN: string, resolve: any, reject: any) => void
+    validate_cPlus: (FEN: string) => Promise<string>,
+    validateAndMakeMove_cPlus: (FEN: string) => Promise<string>
 } = bindings('chessFunctions');
 
-export function validate(FEN: string): Promise<string> {
+export const validate: (FEN: string) => Promise<string> = chessFunctions.validate_cPlus;
+export const validateAndMakeMove: (FEN: string) => Promise<string> = chessFunctions.validateAndMakeMove_cPlus;
 
-    return new Promise((resolve, reject) => {
-        chessFunctions.validate_cPlus(FEN, resolve, reject);
-    });
-}
 
-export function validateAndMakeMove(FEN: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        chessFunctions.validateAndMakeMove_cPlus(FEN, resolve, reject);
-
-    });
-}
 
 
 
