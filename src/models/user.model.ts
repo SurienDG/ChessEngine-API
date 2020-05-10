74958d4104090b439d6fa1a1981dd1eaebb60eb4
Daniel Bergin <daniel.bergin56@gmail.com>
2020-05-10 14:51:29 -0400

    modified user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,7 +2,7 @@
 import  mongoose = require('mongoose');
 
 const user = 'dbAdmin';
-const password = 'Ps5@BTqPHBqb';
+const password = 'gYxZ1aNKL1HK9D8O';
 const database = 'usersdb';
 const server = 'cluster0-mjmqj.azure.mongodb.net';
 
@@ -12,7 +12,7 @@ mongoose.connect(`mongodb+srv://${user}:${password}@${server}/${database}?retryW
 
 const UserSchema = new mongoose.Schema({
     userName: String,
-    gameid: Number,
+    gameid: {type: Number, default: 0},
     FEN: { type: String, default: 'newGameString'},
     AIOn: { type: Boolean, default: false}
 });
