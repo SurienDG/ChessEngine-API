2fe6625564bd4de1b9698b7a14127ac585c8b76b
SurienDG <surien.das-giwojno@uwaterloo.ca>
2020-09-04 00:54:39 -0400

    Fix code formatting for .cc file

--- src/ChessFunctions/chessJS.cc
+++ src/ChessFunctions/chessJS.cc
@@ -1,34 +1,33 @@
-#include "napi.h"
-
 #include <chrono>  // std::chrono::seconds
 #include <thread>
 
 #include "json.hpp"
+#include "napi.h"
 #include "promiseWrapper.h"
 
 using namespace nlohmann;
 
 Napi::Promise validateAndMakeMoveJS(const Napi::CallbackInfo &info) {
-    return PROMISE(info, {
-        std::string FEN = info[0].As<Napi::String>();
-        resolve(FEN);
-    });
+  return PROMISE(info, {
+    std::string FEN = info[0].As<Napi::String>();
+    resolve(FEN);
+  });
 }
 
 Napi::Promise validationJS(const Napi::CallbackInfo &info) {
-    return PROMISE(info, {
-        std::string FEN = info[0].As<Napi::String>();
-        // Add main code here
-        resolve(FEN);
-    });
+  return PROMISE(info, {
+    std::string FEN = info[0].As<Napi::String>();
+    // Add main code here
+    resolve(FEN);
+  });
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports.Set(Napi::String::New(env, "validateAndMakeMove_cPlus"),
-                Napi::Function::New(env, validateAndMakeMoveJS));
-    exports.Set(Napi::String::New(env, "validate_cPlus"),
-                Napi::Function::New(env, validationJS));
-    return exports;
+  exports.Set(Napi::String::New(env, "validateAndMakeMove_cPlus"),
+              Napi::Function::New(env, validateAndMakeMoveJS));
+  exports.Set(Napi::String::New(env, "validate_cPlus"),
+              Napi::Function::New(env, validationJS));
+  return exports;
 }
 
 NODE_API_MODULE(chessFunctions, Init)
